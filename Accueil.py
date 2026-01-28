9690678ae41638cd9f9d8654a0f3b4fd4c0cbefc
Benji11437 <159194518+Benji11437@users.noreply.github.com>
2026-01-28 03:15:03 +0100

    Add files via upload

---
+++ Accueil.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Analyse des tweets",
+    layout="wide"
+)
+
+st.title("Projet Développez une preuve de concept – Analyse des tweets")
+
+st.markdown("""
+Bienvenue dans l'application d'analyse de sentiments.
+
+Utilisez le menu à gauche pour naviguer :
+- **Analyse exploratoire**
+- **Prédiction de sentiment**
+""")
